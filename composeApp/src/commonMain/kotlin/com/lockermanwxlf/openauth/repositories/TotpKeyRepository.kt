fde62ccec25cbe03801a4fd52228c5aad7cff089
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-09-15 02:32:33 -0500

    Add addTotpKey

--- composeApp/src/commonMain/kotlin/com/lockermanwxlf/openauth/repositories/TotpKeyRepository.kt
+++ composeApp/src/commonMain/kotlin/com/lockermanwxlf/openauth/repositories/TotpKeyRepository.kt
@@ -1,9 +1,16 @@
 package com.lockermanwxlf.openauth.repositories
 
+import com.lockermanwxlf.openauth.persistence.TotpKey
 import com.lockermanwxlf.openauth.persistence.TotpKeyDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class TotpKeyRepository(
     private val totpKeyDao: TotpKeyDao
 ) {
     fun getTotpKeysFlow() = totpKeyDao.getAllAsFlow()
+
+    suspend fun addTotpKey(totpKey: TotpKey) = withContext(Dispatchers.IO) {
+        totpKeyDao.upsertAll(totpKey)
+    }
 }
