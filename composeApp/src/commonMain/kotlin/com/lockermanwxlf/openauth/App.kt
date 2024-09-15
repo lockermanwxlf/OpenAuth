2ec43979d4c5dbae92b37c9a97bb33f8f7bfc94c
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-09-15 02:35:33 -0500

    Show HomeScreen

--- composeApp/src/commonMain/kotlin/com/lockermanwxlf/openauth/App.kt
+++ composeApp/src/commonMain/kotlin/com/lockermanwxlf/openauth/App.kt
@@ -1,25 +1,19 @@
 package com.lockermanwxlf.openauth
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import com.lockermanwxlf.openauth.screens.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import openauth.composeapp.generated.resources.Res
-import openauth.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-
+        Surface(modifier = Modifier.fillMaxSize()) {
+            HomeScreen()
+        }
     }
 }
