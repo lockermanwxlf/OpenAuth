481728d873704b30c7f3c89ca0e97ff4b35dfbd7
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-09-16 09:44:57 -0500

    Add AddTotpKeyViewModel to commonModule

--- composeApp/src/commonMain/kotlin/com/lockermanwxlf/openauth/di/KoinModules.kt
+++ composeApp/src/commonMain/kotlin/com/lockermanwxlf/openauth/di/KoinModules.kt
@@ -2,6 +2,7 @@ package com.lockermanwxlf.openauth.di
 
 import com.lockermanwxlf.openauth.persistence.TotpKeyDatabase
 import com.lockermanwxlf.openauth.repositories.TotpKeyRepository
+import com.lockermanwxlf.openauth.viewmodels.AddTotpKeyViewModel
 import com.lockermanwxlf.openauth.viewmodels.HomeViewModel
 import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.core.context.startKoin
@@ -14,6 +15,7 @@ val commonModule = module {
     single { get<TotpKeyDatabase>().getTotpKeyDao() }
     singleOf(::TotpKeyRepository)
     viewModelOf(::HomeViewModel)
+    viewModelOf(::AddTotpKeyViewModel)
 }
 
 expect val platformModule: Module
